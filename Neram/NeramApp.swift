2c9600367a4ce034e59331d6e54f053e006bcde1
Selvarajan Thangavel <selvarajan.thangavel@d4t4solutions.com>
2021-12-13 22:27:24 +0530

    Initial Commit

---
+++ Neram/NeramApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeramApp.swift
+//  Neram
+//
+//  Created by Selvarajan on 13/12/21.
+//
+
+import SwiftUI
+
+@main
+struct NeramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
